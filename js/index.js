12452ebe0778b6f2eed542be902dfa73830831f6
Christopher Bailey <seabayley@outlook.com>
2019-09-03 17:45:56 -0500

    Added first two event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.querySelectorAll('.nav-link').forEach(elem => {
+    elem.addEventListener('mouseenter', event => {
+        elem.style.transform = 'scale(1.2)';
+    });
+
+    elem.addEventListener('mouseleave', event => {
+        elem.style.transform = 'scale(1.0)';
+    });
+});
